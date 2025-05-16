0a3adfd0df9c5698ff2b5f8fda7e1a5e2e716c53
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2025-05-16 17:51:01 +0300

    refactor: add fallback for empty results

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -16,9 +16,14 @@ const Recipes = async ({ searchParams }: RecipesPageProps) => {
   return (
     <div className="px-2 sm:px-5 md:px-11 max-w-[1620px] mx-auto flex flex-col gap-8 py-12">
       <div className="flex flex-wrap justify-center gap-4 p-4">
-        {recipesData.results.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        ))}
+        {recipesData.results.length ? (
+          recipesData.results.map((recipe) => <RecipeCard key={recipe.id} recipe={recipe} />)
+        ) : (
+          <div className="flex flex-col items-center justify-center text-center text-gray-500 py-12">
+            <p className="text-5xl font-semibold">No recipes found 😕</p>
+            <p className="mt-2 text-2xl text-gray-400">Try adjusting your search filters or keywords.</p>
+          </div>
+        )}
       </div>
     </div>
   );
