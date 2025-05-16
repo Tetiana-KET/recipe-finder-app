a0ea41271b4e5903029b7f77bd47fac273fb77fe
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2025-05-16 17:49:58 +0300

    refactor: determine current route to style current page link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Link from 'next/link';
 import { NAVIGATION_LIST } from '@/consts/navigationList';
+import { usePathname } from 'next/navigation';
 
 interface NavigationListItem {
   id: string;
@@ -8,13 +11,17 @@ interface NavigationListItem {
 }
 
 export const NavBar = () => {
+  const pathName = usePathname();
+  const isActive = (href: string) => pathName === href;
   return (
     <nav className="h-full flex justify-between items-center">
       {NAVIGATION_LIST.map((item: NavigationListItem) => (
         <Link
           key={item.id}
           href={item.path}
-          className="inline-flex items-center h-full px-2 text-lg transition-colors hover:bg-black/10"
+          className={`inline-flex items-center h-full px-2 text-lg 
+            transition-colors duration-300 ease-in-out 
+            hover:bg-black/30 ${isActive(item.path) ? 'bg-black/10 pointer-events-none' : ''}`}
         >
           {item.title}
         </Link>
